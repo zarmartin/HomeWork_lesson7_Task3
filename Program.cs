feddb43efc37ae06d88f1544cb862ca334a0b89e
Martin Zarutski <martinzar5@gmail.com>
2023-06-26 15:09:52 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+
+ArrayList sitiesOfBelarus = new ArrayList() { "Minsk", "Grodno", "Mogilev", "Gomel", "Vitebsk"};
+
+foreach(object o in sitiesOfBelarus)
+{
+    Console.WriteLine(o);
+}
+
+Console.WriteLine(Environment.NewLine);
+
+
+sitiesOfBelarus.AddRange(new string[] { "Gorki", "Borisov" });
+
+foreach(object o in sitiesOfBelarus)
+{
+    Console.WriteLine(o);
+}
+
+Console.WriteLine(Environment.NewLine);
+
+sitiesOfBelarus.RemoveAt(3);
+
+foreach(object o in sitiesOfBelarus)
+{
+    Console.WriteLine(o);
+}
+
+sitiesOfBelarus.Remove("Gorki");
+Console.WriteLine(Environment.NewLine);
+foreach (object o in sitiesOfBelarus)
+{
+    Console.WriteLine(o);
+}
+
+sitiesOfBelarus.Sort();
+Console.WriteLine(Environment.NewLine);
+foreach(object o in sitiesOfBelarus)
+{
+    Console.WriteLine(o);
+}
